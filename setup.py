d74f57867bf87a57523ed52c2850e27ba080a4bc
Avinash Tulasi <avinasht@iiitd.ac.in>
2019-10-27 12:57:56 +0530

    removing redundancies

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from distutils.core import setup
 setup(
   name = 'TikPy',         # How you named your package folder (MyLib)
   packages = ['TikPy'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Tiktok API. ',   # Give a short description about your library
   author = 'Vishwesh Kumar',                   # Type in your name
   author_email = 'vishwesh18119@iiitd.ac.in',      # Type in your E-Mail
   url = 'https://github.com/precog-recr/TikPy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/precog-recr/TikPy/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/precog-recr/TikPy/archive/V_02.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'json',
           'bs4',
           'newspaper3k'
       ],
--- tester.py
+++
@@ -1,3 +0,0 @@
-from TikPy import API
-
-print(API.get_user_info('https://www.tiktok.com/@avikdutta', from_video=False))
